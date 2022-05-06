dd7202b765e0538729c306b27c28341a77f3c695
Patitotective <cristobalriaga@gmail.com>
2022-05-05 20:32:13 -0500

    Fixed appimage download and added arch config key

--- nakefile.nim
+++ nakefile.nim
@@ -16,7 +16,7 @@ const
 
   X-AppImage-Name=$name
   X-AppImage-Version=$version
-  X-AppImage-Arch=x86_64
+  X-AppImage-Arch=$arch
   """.dedent()
 let
   config = configPath.readPrefs()
@@ -28,6 +28,7 @@ let
     config["fontPath"].getString()
   ]
   name = config["name"].getString() 
+  arch = if "arch" in config: config["arch"].getString() else: "x86_64"
 
 task "build", "Build AppImage":
   shell "nimble install -d -y"
@@ -43,26 +44,27 @@ task "build", "Build AppImage":
       "name", name, 
       "categories", config["categories"].getSeq().mapIt(it.getString()).join(";"), 
       "version", config["version"].getString(), 
-      "comment", config["comment"].getString()
+      "comment", config["comment"].getString(), 
+      "arch", arch
     ]
   )
   copyFile(config["svgIconPath"].getString(), "AppDir" / &"{name}.svg")
 
-  var appimagetoolPath = "appimagetool"
+  var appimagetoolPath = "appimagetoola"
   if not silentShell("Checking for appimagetool", appimagetoolPath, "--help"):
       appimagetoolPath = "./appimagetool-x86_64.AppImage"
       if not fileExists(appimagetoolPath):
         direSilentShell &"Dowloading {appimagetoolPath}", "wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O ", appimagetoolPath
       shell "chmod +x", appimagetoolPath
 
-  withDir "AppDir":
-    if "ghRepo" in config:
-      echo "Building updateable AppImage"
-      let ghInfo = config["ghRepo"].getString().split('/')
-      direShell appimagetoolPath, "-u", &"\"gh-releases-zsync|{ghInfo[0]}|{ghInfo[1]}|latest|{name}-*.AppImage.zsync\"", "."
-    else:
-      echo &"ghRepo key not in {configPath}. Skipping updateable AppImage"
-      direShell appimagetoolPath, "."
+  if "ghRepo" in config:
+    echo "Building updateable AppImage"
+    let ghInfo = config["ghRepo"].getString().split('/')
+    direShell appimagetoolPath, "-u", &"\"gh-releases-zsync|{ghInfo[0]}|{ghInfo[1]}|latest|{name}-*.AppImage.zsync\"", ".", &"AppDir/{name}-{arch}.AppImage"
+  else:
+    echo &"ghRepo key not in {configPath}. Skipping updateable AppImage"
+    direShell appimagetoolPath, "."
+
   echo "Succesfully built AppImage at AppDir/"
 
 task "run", "Build (if needed) and run AppImage":
